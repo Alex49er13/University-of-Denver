00e3812012689191fc9985ecf9090033bf650c64
Sierra R. Mills <Alex49er13@gmail.com>
2019-04-15 02:58:06 +0000

    contribute and about links

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,6 +4,28 @@ class QuotesController < ApplicationController
   end
 
   def new
-     @quote = Quote.new
+   @quote = Quote.new 
   end
+
+  def create
+    Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+   end
+    redirect_to root_path
+    end
+
+def about
+
+
+end
+private
+
+def quote_params
+   params.require(:quote).permit(:saying, :author)
+end
+
+
+
+
 end
